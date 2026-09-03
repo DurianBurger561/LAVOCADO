907f065fe23925b4f55cc07ea98ae7fcafecda87
DurianBurger561 <x12468xl@outlook.com>
2026-09-03 14:33:26 +1000

    add detector

--- app/detector.py
+++ app/detector.py
@@ -1,7 +1,34 @@
-# Vision Detector
-# Receive a photo, detect it with NudeNet. According the rule of config to make choose whther it 
-# need to block
-
+"""
+Vision Detector
+Receive a photo, detect it with NudeNet. According the rule of config to make choose whther it 
+need to block. Returing the spot that trigger the block
+"""
 from nudenet import NudeDetector
 from app import config
 
+
+class Detector:
+
+    def __init__(self):
+      self.model = NudeDetector()
+
+    def check(self, image):
+       """checking a photo, make decision whether to block
+          image: photo path
+       """
+       check_points = self.model.detect(image) # return the list of dictionaries ,each dict is body parts
+
+       for element in check_points:
+          body_part = element["class"]
+          score = element["score"]
+
+          if body_part in config.BLOCK_LABELS: # whether it's a block part
+             is_block_part = True
+
+          if score >= config.DETECTION_THRESHOLD:
+             is_over_threshold = True
+
+          if is_block_part and is_over_threshold:
+             return {
+                "blocked": True,
+             }
